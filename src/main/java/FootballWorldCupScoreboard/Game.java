24ba31ef78970291a5423476bcd7f642ce3382ee
bartek <bartek_wojnarowski@o2.l>
2025-01-20 21:05:20 +0100

    [TEST][FEAT] Modify Game constructor for edge cases and write test to edge cases: * When teams are null * When teams are empty * When teams are the same * When team name is too long

--- src/main/java/FootballWorldCupScoreboard/Game.java
+++ src/main/java/FootballWorldCupScoreboard/Game.java
@@ -3,22 +3,57 @@ package FootballWorldCupScoreboard;
 
 public class Game {
 
-    private String homeTeam;
+    private final String homeTeam;
 
-    private String awayTeam;
+    private final String awayTeam;
 
     private int homeScore;
 
     private int awayScore;
 
+    private static final String HOME_TEAM_ERROR_MESSAGE = "Home team cannot be null or empty.";
+
+    private static final String AWAY_TEAM_ERROR_MESSAGE = "Away team cannot be null or empty.";
+
+    private static final String SAME_TEAMS_ERROR_MESSAGE = "Home team and away team cannot be the same.";
+
+    private static final String TOO_LONG_TEAM_NAME = "Team name cannot be longer than 35 characters.";
+
     public Game(String homeTeam, String awayTeam) {
+        validateTeams(homeTeam, awayTeam);
+
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
+    }
+
+    private void validateTeams(String homeTeam, String awayTeam) {
+        validateIfTeamSetIsCorrect(homeTeam, awayTeam);
+        validateTeamLenght(homeTeam, awayTeam);
+    }
+
+    private static void validateTeamLenght(String homeTeam, String awayTeam) {
+        if (homeTeam.trim().length() > 35 || awayTeam.trim().length() > 35) {
+            throw new IllegalArgumentException(TOO_LONG_TEAM_NAME);
+        }
     }
 
     public void startGame() {
         this.homeScore = 0;
         this.awayScore = 0;
+    }
+
+    private void validateIfTeamSetIsCorrect(String homeTeam, String awayTeam) {
+        if (homeTeam == null || homeTeam.trim().isEmpty()) {
+            throw new IllegalArgumentException(HOME_TEAM_ERROR_MESSAGE);
+        }
+
+        if (awayTeam == null || awayTeam.trim().isEmpty()) {
+            throw new IllegalArgumentException(AWAY_TEAM_ERROR_MESSAGE);
+        }
+
+        if (homeTeam.equals(awayTeam)) {
+            throw new IllegalArgumentException(SAME_TEAMS_ERROR_MESSAGE);
+        }
     }
 
     public int getHomeScore() {
--- src/test/java/FootballWorldCupScoreboard/GameTest.java
+++ src/test/java/FootballWorldCupScoreboard/GameTest.java
@@ -4,19 +4,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GameTest {
 
     private Game game;
 
-    @BeforeEach
-    void setup() {
-        //Create new game, before each test
-        game = new Game("Poland", "Spain");
-    }
+    private static final String HOME_TEAM_ERROR_MESSAGE = "Home team cannot be null or empty.";
+
+    private static final String AWAY_TEAM_ERROR_MESSAGE = "Away team cannot be null or empty.";
+
+    private static final String SAME_TEAMS_ERROR_MESSAGE = "Home team and away team cannot be the same.";
+
+    private static final String TOO_LONG_TEAM_NAME = "Team name cannot be longer than 35 characters.";
+
+    private static final String POLAND = "Poland";
+
+    private static final String SPAIN = "Spain";
 
     @Test
     void shouldStartGameWithInitialScoreZero() {
+        //given
+        // New Game object
+        Game game = new Game(POLAND, SPAIN);
+
         //when
         // Call startGame method
         game.startGame();
@@ -25,5 +36,114 @@ public class GameTest {
         // Check if score is set to 0-0
         assertEquals(0, game.getHomeScore());
         assertEquals(0, game.getAwayScore());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenHomeTeamIsNull() {
+        //when
+        // Create wrong Game object
+        // Should throw IllegalArgumentException with correct message
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Game(null, SPAIN) // Wyjątek rzucany w konstruktorze
+        );
+
+        assertEquals(HOME_TEAM_ERROR_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenAwayTeamIsNull() {
+        //when
+        // Create wrong Game object
+        // Should throw IllegalArgumentException with correct message
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Game(POLAND, null)
+        );
+
+        assertEquals(AWAY_TEAM_ERROR_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenAwayTeamIsEmpty() {
+        //when
+        // Create wrong Game object
+        // Should throw IllegalArgumentException with correct message
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Game(POLAND, "")
+        );
+
+        assertEquals(AWAY_TEAM_ERROR_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenHomeTeamIsEmpty() {
+        //when
+        // Create wrong Game object
+        // Should throw IllegalArgumentException with correct message
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Game("", SPAIN)
+        );
+
+        assertEquals(HOME_TEAM_ERROR_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExcepetionWhenBothTeamsAreNull() {
+        //when
+        // Create wrong Game object
+        // Should throw IllegalArgumentException with correct message
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Game(null, null)
+        );
+
+        assertEquals(HOME_TEAM_ERROR_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExcepetionWhenBothTeamsAreEmpty() {
+        //when
+        // Create wrong Game object
+        // Should throw IllegalArgumentException with correct message
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Game("", "")
+        );
+
+        assertEquals(HOME_TEAM_ERROR_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenTeamsAreTheSame() {
+        //when
+        // Create wrong Game object
+        // Should throw IllegalArgumentException with correct message
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Game(POLAND, POLAND)
+        );
+
+        assertEquals(SAME_TEAMS_ERROR_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenHomeTeamNameExceedsMaxLength() {
+        //given
+        // Too long away team name
+        String longName = POLAND.repeat(6);
+
+        //when
+        // Create game with too long home team name
+        //then
+        // Should throw IllegalArgumentException with correct message
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Game(longName, SPAIN)
+        );
+
+        assertEquals(TOO_LONG_TEAM_NAME, exception.getMessage());
     }
 }
