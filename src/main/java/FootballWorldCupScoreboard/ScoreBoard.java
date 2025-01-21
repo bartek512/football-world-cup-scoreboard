2eda1717db9d2f9b656f9d634442e6bb4d9f1985
bartek <bartek_wojnarowski@o2.l>
2025-01-21 22:26:03 +0100

    [TEST][FEAT] Add tests for edge cases in ScoreBoardTest, and implement handling of these edge cases: * Can't start a game, when both teams are already playing match with each other * Can't start game, when one of teams is already playng a match

--- src/main/java/FootballWorldCupScoreboard/ScoreBoard.java
+++ src/main/java/FootballWorldCupScoreboard/ScoreBoard.java
@@ -7,11 +7,39 @@ public class ScoreBoard {
 
     private List<Game> games = new ArrayList<>();
 
+    private static final String GAME_EXISTS_ERROR = "A game between these teams already exists.";
+
+    private static final String TEAM_BUSY_ERROR = "%s team is already playing another game.";
+
     public void startGame(String homeTeam, String awayTeam) {
+        if (validateIfGameExists(homeTeam, awayTeam)) {
+            throw new IllegalArgumentException(GAME_EXISTS_ERROR);
+        }
+
+        if (isTeamBusy(homeTeam)) {
+            throw new IllegalArgumentException(String.format(TEAM_BUSY_ERROR, homeTeam));
+        }
+
+        if (isTeamBusy(awayTeam)) {
+            throw new IllegalArgumentException(String.format(TEAM_BUSY_ERROR, awayTeam));
+        }
+
         games.add(new Game(homeTeam, awayTeam));
     }
 
     public List<Game> getGames() {
         return games;
+    }
+
+    private boolean validateIfGameExists(String homeTeam, String awayTeam) {
+        return games.stream().anyMatch(game -> game.getHomeTeam().equals(homeTeam) &&
+                game.getAwayTeam().equals(awayTeam)) || games.stream().anyMatch(
+                game -> game.getHomeTeam().equals(awayTeam) &&
+                        game.getAwayTeam().equals(homeTeam));
+    }
+
+    private boolean isTeamBusy(String team) {
+        return games.stream()
+                .anyMatch(game -> game.getHomeTeam().equals(team) || game.getAwayTeam().equals(team));
     }
 }
--- src/test/java/FootballWorldCupScoreboard/ScoreBoardTest.java
+++ src/test/java/FootballWorldCupScoreboard/ScoreBoardTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ScoreBoardTest {
 
@@ -14,6 +15,12 @@ public class ScoreBoardTest {
     private static final String POLAND = "Poland";
 
     private static final String SPAIN = "Spain";
+
+    private static final String ITALY = "Italy";
+
+    private static final String GAME_EXISTS_ERROR = "A game between these teams already exists.";
+
+    private static final String TEAM_BUSY_ERROR = "%s team is already playing another game.";
 
     @BeforeEach
     void setUp() {
@@ -32,5 +39,77 @@ public class ScoreBoardTest {
         assertEquals(SPAIN, games.get(0).getAwayTeam());
         assertEquals(0, games.get(0).getHomeScore());
         assertEquals(0, games.get(0).getAwayScore());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenTheSameGameExists() {
+        // when
+        // Start new game
+        scoreBoard.startGame(POLAND, SPAIN);
+
+        //and
+        // Try to start game, between the same teams
+        //then
+        // Should exception with expected message
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> scoreBoard.startGame(POLAND, SPAIN)
+        );
+
+        assertEquals(GAME_EXISTS_ERROR, exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenTheSameGameExistsButTeamsAreSwitched() {
+        // when
+        // Start new game
+        scoreBoard.startGame(POLAND, SPAIN);
+
+        //and
+        // Try to start game, between the same teams
+        //then
+        // Should exception with expected message
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> scoreBoard.startGame(SPAIN, POLAND)
+        );
+
+        assertEquals(GAME_EXISTS_ERROR, exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenHomeTeamIsBusy() {
+        // when
+        // Start new game
+        scoreBoard.startGame(POLAND, SPAIN);
+
+        //and
+        // Try to start game with one busy team
+        //then
+        // Should exception with expected message
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> scoreBoard.startGame(POLAND, ITALY)
+        );
+
+        assertEquals(String.format(TEAM_BUSY_ERROR, POLAND), exception.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenAwayTeamIsBusy() {
+        // when
+        // Start new game
+        scoreBoard.startGame(POLAND, SPAIN);
+
+        //and
+        // Try to start game with one busy team
+        //then
+        // Should exception with expected message
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> scoreBoard.startGame(ITALY, SPAIN)
+        );
+
+        assertEquals(String.format(TEAM_BUSY_ERROR, SPAIN), exception.getMessage());
     }
 }
